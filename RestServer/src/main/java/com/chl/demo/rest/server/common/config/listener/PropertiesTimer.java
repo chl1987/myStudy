b618dd03c9126997e11e256b06100a64a030978e
chl1987 <caohuilin@vip.qq.com>
2017-04-01 18:06:02 +0800

    Create PropertiesTimer.java

---
+++ RestServer/src/main/java/com/chl/demo/rest/server/common/config/listener/PropertiesTimer.java
@@ -0,0 +1,40 @@
+package com.chl.demo.rest.server.common.config.listener;
+
+import javax.servlet.ServletContext;
+import java.util.Timer;
+
+/**
+ * 配置加载执行器
+ * Created by caodongdong on 2017-03-30.
+ */
+public class PropertiesTimer
+{
+    private final Timer timer = new Timer();
+
+    private final int sec;
+
+    private ServletContext context = null;
+
+    public PropertiesTimer(int seconds, ServletContext context)
+    {
+        sec = seconds;
+        this.context = context;
+    }
+
+    /**
+     * 启动自动监听任务
+     * {@code sec}秒之后启动，此后每{@code sec}执行一次
+     */
+    public void start()
+    {
+        timer.schedule(new PropertiesTask(this.context), sec * 1000, sec * 1000);
+    }
+
+    /**
+     * 停止自动监听任务
+     */
+    public void stop()
+    {
+        timer.cancel();
+    }
+}
