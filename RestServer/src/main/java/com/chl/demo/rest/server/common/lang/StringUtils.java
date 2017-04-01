f7adeb9edfff99921f42db7465354781edfa7b14
chl1987 <caohuilin@vip.qq.com>
2017-04-01 18:12:37 +0800

    Create StringUtils.java

---
+++ RestServer/src/main/java/com/chl/demo/rest/server/common/lang/StringUtils.java
@@ -0,0 +1,68 @@
+package com.chl.demo.rest.server.common.lang;
+
+/**
+ * 字符串工具
+ * Created by caodongdong on 2017-03-30.
+ */
+public class StringUtils
+{
+    public static boolean isEmpty(String str)
+    {
+        return ((str == null) || (str.trim().length() == 0));
+    }
+
+    public static boolean isNotEmpty(String str)
+    {
+        return !isEmpty(str);
+    }
+
+    /**
+     * 只要有一个是空，就返回true
+     */
+    public static boolean isEmpty(String... strs)
+    {
+        for (String str : strs)
+        {
+            if (isEmpty(str))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 只要有一个是空，就返回true
+     */
+    public static boolean isEmpty(Object... strs)
+    {
+        for (Object str : strs)
+        {
+            if (isEmpty(str))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 只要有一个是空，就返回true
+     */
+    public static boolean isEmpty(Object str)
+    {
+        if (str == null)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 全部都不为空才返回true
+     */
+    public static boolean isNotEmpty(String... strs)
+    {
+        return !isEmpty(strs);
+    }
+}
