47bcb03b7228264eec0bddc43e1cbf60cdd42202
chl1987 <caohuilin@vip.qq.com>
2017-04-01 18:18:34 +0800

    Update init sql.sql

--- RestServer/src/main/resources/init sql.sql
+++ RestServer/src/main/resources/init sql.sql
@@ -8,3 +8,14 @@ CREATE TABLE `t_student` (
   `name` varchar(255) COLLATE utf8_bin DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+
+
+CREATE TABLE `lockentry` (
+  `lockId` varchar(32) NOT NULL DEFAULT '',
+  `status` int(1) NOT NULL DEFAULT '0',
+  `timeOut` int(4) NOT NULL DEFAULT '0',
+  `updateTime` varchar(32) DEFAULT NULL,
+  `version` bigint(20) NOT NULL,
+  PRIMARY KEY (`lockId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
